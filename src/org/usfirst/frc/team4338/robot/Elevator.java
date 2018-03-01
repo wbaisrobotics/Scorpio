3a1e3cb0d70955b301dfdd56096b5ba80ec9bceb
oleitersdorf <31508502+oleitersdorf@users.noreply.github.com>
2018-03-01 20:59:55 +0200

    Fork class completed, and verified

--- src/org/usfirst/frc/team4338/robot/Elevator.java
+++ src/org/usfirst/frc/team4338/robot/Elevator.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4338.robot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -13,10 +14,11 @@ public class Elevator {
 	
 	public static final double MAX_HEIGHT = 1000; // in pulses
 	
-	public Elevator (int talonPort, int limitSwitchPort, int encoderA, int encoderB){
-		bottomLimitSW = new DigitalInput (limitSwitchPort);
-		motor = new WPI_TalonSRX (talonPort);
-		encoder = new Encoder (encoderA, encoderB);
+	public Elevator(int talonPort, int limitSwitchPort, int encoderA, int encoderB){
+		this.motor = new WPI_TalonSRX(talonPort);
+		this.motor.setNeutralMode(NeutralMode.Brake);
+		this.encoder = new Encoder(encoderA, encoderB);
+		this.bottomLimitSW = new DigitalInput(limitSwitchPort);
 	}
 
 	public double getCurrentHeight() {
@@ -29,15 +31,15 @@ public class Elevator {
 	
 	public boolean atBottom() {
 		// True reading repesents the bottom limit switch being pressed
-		return bottomLimitSW.get() || (getCurrentHeight()<=0);
+		return bottomLimitSW.get() ||(getCurrentHeight()<=0);
 	}
 	
-	public void elevateUpDown (double speed) {
-		if (atBottom() && (speed > 0)) {
+	public void elevateUpDown(double speed) {
+		if(atBottom() &&(speed > 0)) {
 			this.motor.stopMotor();
 			//resetCurrentHeight();
 		}
-		else if ((getCurrentHeight() >= MAX_HEIGHT) && (speed < 0)) {
+		else if((getCurrentHeight() >= MAX_HEIGHT) &&(speed < 0)) {
 			this.motor.stopMotor();
 		}
 		else {
@@ -46,7 +48,7 @@ public class Elevator {
 	}
 
 	public void stop() {
-		elevateUpDown (0);
+		elevateUpDown(0);
 	}
 
 }
---
+++ src/org/usfirst/frc/team4338/robot/Fork.java
@@ -0,0 +1,108 @@
+package org.usfirst.frc.team4338.robot;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public class Fork {
+
+	// ----------- Extension -----------
+	private WPI_TalonSRX motor;
+	private DigitalInput extendedLimitSW;
+	private DigitalInput retractedLimitSW;
+	private Timer timer;
+	
+	// ----------- Releasing fork -----------
+	private DoubleSolenoid releaser;
+	
+	// ----------- Gripping cubes -----------
+	private DoubleSolenoid gripper;
+	
+	// Constructor
+	public Fork(int motorPort, int extendedLimitSwitchPort, int retractedLimitSwitchPort,
+			int gripperPistonA, int gripperPistonB, int releasePistonA, int releasePistonB) {
+		this.motor = new WPI_TalonSRX(motorPort);
+		this.motor.setNeutralMode(NeutralMode.Brake);
+		this.retractedLimitSW = new DigitalInput(retractedLimitSwitchPort);
+		this.retractedLimitSW = new DigitalInput(extendedLimitSwitchPort);
+		
+		this.releaser = new DoubleSolenoid(releasePistonA, releasePistonB);
+		
+		this.gripper = new DoubleSolenoid(gripperPistonA, gripperPistonB);
+		
+	}
+
+	private boolean canExtend() {
+		return !extendedLimitSW.get();
+	}
+
+	private boolean canRetract() {
+		return !retractedLimitSW.get();
+	}
+
+	public void extend() {
+		if(canExtend()) {
+			this.motor.set(1.0);
+		}
+		else {
+			stop();
+		}
+	}
+
+	public void retract() {
+		if(canRetract()) {
+			this.motor.set(-1.0);
+		}
+		else {
+			stop();
+		}
+	}
+
+	public void stop() {
+		this.motor.stopMotor();
+	}
+	
+	//maybeWorks
+	public void extendToReleasePosition() {
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			public void run() {
+				extend();
+			}
+			
+		}, 0, 20);
+		timer.schedule(new TimerTask() {
+			public void run() {
+				timer.cancel();
+			}
+			
+		}, 5000);
+	}
+	
+	// ----------- Releasing fork -----------
+
+	public void releaseFork() {
+		this.releaser.set(Value.kForward);
+	}
+
+	public void holdFork() {
+		this.releaser.set(Value.kReverse);
+	}
+
+	// ----------- Gripping cubes -----------
+	
+	public void openGripper() {
+		this.gripper.set(Value.kReverse);
+	}
+
+	public void closeGripper() {
+		this.gripper.set(Value.kForward);
+	}
+
+}
